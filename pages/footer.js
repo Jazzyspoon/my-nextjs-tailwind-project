28ae8279a137fc4cedecda18ba67e6f1e1bdadaf
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-11-03 14:12:24 -0600

    updated stylings on footer and header

--- pages/footer.js
+++ pages/footer.js
@@ -1,35 +1,40 @@
 import React from "react";
-
+import { Menu } from "@headlessui/react";
 export function Footer() {
   return (
     <footer id="footer" className="bg-gray-800">
       <div className="container mx-auto px-2 sm:px-6 lg:px-8">
-        <div className="relative flex items-center justify-between h-16">
+        <Menu className="relative flex items-center justify-between h-16">
           <ul className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
             <li>
-              <a className="px-3 py-2 rounded-md text-sm font-medium bg-gray-800 p-1  text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+              <Menu.Button
+                className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                className="px-3 py-2 rounded-md text-sm font-medium bg-gray-800 p-1  text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              >
                 Copyright © {new Date().getFullYear()} J Pottorff
-              </a>
+              </Menu.Button>
             </li>
 
             <li>
-              <a
+              <Menu.Button
+                className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                 href="#"
                 className="px-3 py-2 rounded-md text-sm font-medium bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
               >
                 About Me
-              </a>
+              </Menu.Button>
             </li>
             <li>
-              <a
+              <Menu.Button
+                className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                 href="#"
                 className="px-3 py-2 rounded-md text-sm font-medium bg-gray-800 p-1  text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
               >
                 Blog
-              </a>
+              </Menu.Button>
             </li>
           </ul>
-        </div>
+        </Menu>
       </div>
     </footer>
   );
